7db27b87df8e0ea578961e2a5cd7849be2ec7375
Dan Gorman <danielhgorman@gmail.com>
2016-07-20 17:00:10 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,24 @@
+recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) { 
+	// var newObj = Object.assign(object, {value})
+	// return newObj
+	 return Object.assign({}, object, { [key]: value })
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { 
+	object[key] = value
+	return object
+}
+
+function deleteFromObjectByKey(object, key) {
+	var copy = Object.assign({}, object);
+	delete copy[key]
+	return copy
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+	delete object.key
+	return object
+}
